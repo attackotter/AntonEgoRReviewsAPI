de96d4e6c3c931b1121fd2ff0ecc00255bc398ca
attackotter <cstearn11@gmail.com>
2021-10-14 18:02:56 -0500

    Add project files.

---
+++ AntonEgoReviewsAPI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AntonEgoReviewsAPI.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [Route("api/home/addrestraunt")]
+        [HttpPost]
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
